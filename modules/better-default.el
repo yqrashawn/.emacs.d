b59da1420a4c1c42567414365c30256ea85bb508
yqrashawn <namy.19@gmail.com>
2018-03-29 09:50:47 +0800

    add .tab .,tab

--- modules/better-default.el
+++ modules/better-default.el
@@ -701,3 +701,37 @@ otherwise it is scaled down."
 (spacemacs/set-leader-keys "xdw" 'delete-trailing-whitespace)
 (setq dired-recursive-deletes 'always)
 (setq dired-recursive-copies 'always)
+
+(defun switch-to-nth-buffer (n)
+  "Switches to nth most recent buffer. Ignores a bunch of stuff."
+  (catch 'tag
+    (mapcar (lambda (b)
+              (unless
+                  (or
+                   (minibufferp b)
+                   (string-match "^ " (buffer-name b))
+                   (string-match "\*" (buffer-name b))
+                   (equal b (current-buffer)))
+                (if (= n 1)
+                    (progn
+                      (switch-to-buffer b)
+                      (throw 'tag nil))
+                  (setq n (- n 1)))))
+            (buffer-list))))
+
+(defun switch-to-most-recent-buffer ()
+  (interactive)
+  (switch-to-nth-buffer 1))
+(defun switch-to-second-most-recent-buffer ()
+  (interactive)
+  (switch-to-nth-buffer 2))
+(defun switch-to-third-most-recent-buffer ()
+  (interactive)
+  (switch-to-nth-buffer 3))
+
+;;fast switching between two buffers
+;; (define-key evil-normal-state-map (kbd "<tab>") 'switch-to-most-recent-buffer)
+
+;;fast switching between three buffers
+(define-key evil-normal-state-map (kbd "<C-tab>") 'switch-to-second-most-recent-buffer)
+(define-key evil-normal-state-map (kbd "<C-s-tab>") 'switch-to-third-most-recent-buffer)
