43bf46bca0f1b331652b20089f39908944abbe57
yqrashawn <namy.19@gmail.com>
2018-06-21 09:14:43 +0800

    mu4e

--- modules/mail.el
+++ modules/mail.el
@@ -27,10 +27,11 @@
   (mu4e-use-fancy-chars t)
   (mu4e-view-show-addresses t)
   (mu4e-view-show-images t)
+  (mu4e-confirm-quit nil)
   (smtpmail-queue-mail nil)
   (smtpmail-queue-dir "~/Maildir/queue/cur")
   :init
-
+  (setq mu4e-html2text-command "iconv -c -t utf-8 | pandoc -f html -t plain")
   (setq evil-emacs-state-modes (seq-remove
    (lambda (index)
      (not (and
@@ -41,10 +42,26 @@
   (global-set-key (kbd "C-x m") 'mu4e-compose-new)
   (spacemacs/set-leader-keys "1" 'mu4e)
   :config
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name  "Unread filtered"
+                :query "flag:unread and not maildir:/INBOX.gitlab and not maildir:/INBOX.github and not maildir:/INBOX.mailinglist.something-user and not maildir:/INBOX.mailinglist.other-user"
+                :key ?n)
+               )
   (defun jcs-view-in-eww (msg)
     (eww-browse-url (concat "file://" (mu4e~write-body-to-html msg))))
   (add-to-list 'mu4e-view-actions '("eww view" . jcs-view-in-eww) t)
-  (add-to-list 'mu4e-view-actions '("jView in browser" . mu4e-action-view-in-browser) t)
+  (add-to-list 'mu4e-view-actions '("view in browser" . mu4e-action-view-in-browser))
+  (setq mu4e-sent-messages-behavior 'delete)
+  (add-hook 'mu4e-view-mode-hook #'visual-line-mode)
+  (setq mail-user-agent 'mu4e-user-agent)
+  (defvaralias 'mu4e-compose-signature 'message-signature)
+  (setq mu4e-completing-read-function 'ivy-completing-read
+        mu4e-confirm-quit nil)
+  (setq mu4e-headers-show-threads nil) ; Use "P" to toggle threading
+  ;; (setq mu4e-compose-signature (get-string-from-file "~/signature.txt"))
+
+  (add-hook 'mu4e-compose-mode-hook 'flyspell-mode)
   (evil-define-key 'normal mu4e-headers-mode-map "d" 'mu4e-headers-mark-for-trash)
   (evil-define-key 'normal mu4e-headers-mode-map "D" 'mu4e-headers-mark-for-delete)
   (evil-define-key 'normal mu4e-headers-mode-map "r" 'mu4e-headers-mark-for-read)
