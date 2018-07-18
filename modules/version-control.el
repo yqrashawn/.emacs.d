322fff5310f904edfc210625a5b649cf61975ff1
yqrashawn <namy.19@gmail.com>
2018-07-18 16:25:17 +0800

    gitgutter to diff-hl

--- modules/version-control.el
+++ modules/version-control.el
@@ -14,42 +14,6 @@
       (shell-command (concat "git add " filename))
       (message "DONE! git add %s" filename))))
 
-(defun my-reshape-git-gutter (gutter)
-  "Re-shape gutter for `ivy-read'."
-  (let* ((linenum-start (aref gutter 3))
-         (linenum-end (aref gutter 4))
-         (target-line "")
-         (target-linenum 1)
-         (tmp-line "")
-         (max-line-length 0))
-    (save-excursion
-      (while (<= linenum-start linenum-end)
-        (goto-line linenum-start)
-        (setq tmp-line (replace-regexp-in-string "^[ \t]*" ""
-                                                 (buffer-substring (line-beginning-position)
-                                                                   (line-end-position))))
-        (when (> (length tmp-line) max-line-length)
-          (setq target-linenum linenum-start)
-          (setq target-line tmp-line)
-          (setq max-line-length (length tmp-line)))
-
-        (setq linenum-start (1+ linenum-start))))
-    ;; build (key . linenum-start)
-    (cons (format "%s %d: %s"
-                  (if (eq 'deleted (aref gutter 1)) "-" "+")
-                  target-linenum target-line)
-          target-linenum)))
-
-(defun my-goto-git-gutter ()
-  (interactive)
-  (if git-gutter:diffinfos
-      (ivy-read "git-gutters:"
-                (mapcar 'my-reshape-git-gutter git-gutter:diffinfos)
-                :action (lambda (e)
-                          (unless (numberp e) (setq e (cdr e)))
-                          (goto-line e)))
-    (message "NO git-gutters!")))
-
 (use-package git-commit
   :straight t)
 (use-package magit-popup
@@ -105,38 +69,13 @@
   (setq magithub-features t
         magithub-feature-autoinject t
         magithub-dir "~/Dropbox/sync/magithub"))
-
-(use-package git-gutter
+
+(use-package diff-hl
   :straight t
-  :diminish git-gutter-mode
-  :init
-  (global-git-gutter-mode)
-  (custom-set-variables '(git-gutter:handled-backends '(git)))
-  (global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
-  (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
-  (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
-  (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
   :config
-  (defun my-goto-next-hunk (arg)
-    (interactive "p")
-    (if (memq major-mode '(diff-mode))
-        (diff-hunk-next)
-      (forward-line)
-      (if (re-search-forward "\\(^<<<<<<<\\|^=======\\|^>>>>>>>\\)" (point-max) t)
-          (goto-char (line-beginning-position))
-        (forward-line -1)
-        (git-gutter:next-hunk arg))))
-  (defun my-goto-previous-hunk (arg)
-    (interactive "p")
-    (if (memq major-mode '(diff-mode))
-        (diff-hunk-prev)
-      (forward-line -1)
-      (if (re-search-backward "\\(^>>>>>>>\\|^=======\\|^<<<<<<<\\)" (point-min) t)
-          (goto-char (line-beginning-position))
-        (forward-line -1)
-        (git-gutter:previous-hunk arg))))
-  (define-key evil-normal-state-map "]h" 'git-gutter:next-hunk)
-  (define-key evil-normal-state-map "[h" 'git-gutter:previous-hunk))
+  (global-diff-hl-mode +1)
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 (use-package git-timemachine
   :straight t
