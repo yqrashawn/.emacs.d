770ba81b9115f38d649a13a3f369a8e3ad996c8c
yqrashawn <namy.19@gmail.com>
2018-08-04 09:00:38 +0800

    add home-imac

--- modules/auto-detect-which-machine.el
+++ modules/auto-detect-which-machine.el
@@ -19,6 +19,15 @@
     (load-theme 'zenburn))
   (spacemacs/set-default-font yq/large-screen-default-font))
 
+(defun yq/home-imac ()
+  "Load macbook theme and font size."
+  (interactive)
+  (if (yq/day-p)
+      ;; (load-theme 'spacemacs-light)
+      (load-theme 'zenburn)
+    (load-theme 'zenburn))
+  (spacemacs/set-default-font yq/huge-screen-default-font))
+
 
 (defun yq/day-p ()
   "Return t/nil if it's day or night."
@@ -26,7 +35,10 @@
   (let ((current-hour (nth 2 (decode-time))))
     (and (> current-hour 8) (< current-hour 18))))
 
-(if (file-exists-p "~/yq.machine.macbook") (yq/macbook) (yq/imac))
+;; (if (file-exists-p "~/yq.machine.macbook") (yq/macbook) (yq/imac))
+(cond ((file-exists-p "~/yq.machine.macbook") (yq/macbook))
+      ((file-exists-p "~/yq.machine.home-imac") (yq/home-imac))
+      (t (yq/imac)))
 
 (provide 'auto-detect-which-machine)
 ;;; auto-detect-which-machine.el ends here
