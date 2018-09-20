674a1bf6da5cb88357d548108b541f780bae894c
yqrashawn <namy.19@gmail.com>
2018-09-20 08:03:07 +0800

    try fix gmail all mail problem

--- modules/mail.el
+++ modules/mail.el
@@ -13,7 +13,7 @@
   (mu4e-maildir-shortcuts
    '(("/gmail/Inbox" . ?i)
      ("/gmail/[Gmail]/Starred" . ?s)
-     ;; ("/gmail/[Gmail]/All Mail" . ?a)
+     ("/gmail/[Gmail]/All Mail" . ?a)
      ("/gmail/[Gmail]/Sent Mail" . ?S)
      ("/gmail/[Gmail]/Trash" . ?d)
      ("/gmail/[Gmail]/Drafts" . ?D)
@@ -43,6 +43,102 @@
   (spacemacs/set-leader-keys "1" 'mu4e)
   (spacemacs/set-leader-keys "2" (lambda () (interactive) ( mu4e~headers-jump-to-maildir "/gmail/INBOX")))
   :config
+  (defvar mu4e-marks
+    '((refile
+       :char ("r" . "▶")
+       :prompt "refile"
+       :dyn-target (lambda (target msg) (mu4e-get-refile-folder msg))
+       :action (lambda (docid msg target) (mu4e~proc-move docid
+                                                          (mu4e~mark-check-target target) "-N")))
+      (delete
+       :char ("D" . "❌")
+       :prompt "Delete"
+       :show-target (lambda (target) "delete")
+       :action (lambda (docid msg target)
+                 (print (mu4e-msg-field msg :message-id))
+                 (yq/mu4e~proc-remove (mu4e-msg-field msg :message-id))))
+      (flag
+       :char ("+" . "✚")
+       :prompt "+flag"
+       :show-target (lambda (target) "flag")
+       :action (lambda (docid msg target) (mu4e~proc-move docid nil "+F-u-N")))
+      (move
+       :char ("m" . "▷")
+       :prompt "move"
+       :ask-target  mu4e~mark-get-move-target
+       :action (lambda (docid msg target) (mu4e~proc-move docid
+                                                          (mu4e~mark-check-target target) "-N")))
+      (read
+       :char    ("!" . "◼")
+       :prompt "!read"
+       :show-target (lambda (target) "read")
+       :action (lambda (docid msg target) (mu4e~proc-move docid nil "+S-u-N")))
+      (trash
+       :char ("d" . "▼")
+       :prompt "dtrash"
+       :dyn-target (lambda (target msg) (mu4e-get-trash-folder msg))
+       :action (lambda (docid msg target) (mu4e~proc-move docid
+                                                          (mu4e~mark-check-target target) "+T-N")))
+      (unflag
+       :char    ("-" . "➖")
+       :prompt "-unflag"
+       :show-target (lambda (target) "unflag")
+       :action (lambda (docid msg target) (mu4e~proc-move docid nil "-F-N")))
+      (untrash
+       :char   ("=" . "▲")
+       :prompt "=untrash"
+       :show-target (lambda (target) "untrash")
+       :action (lambda (docid msg target) (mu4e~proc-move docid nil "-T")))
+      (unread
+       :char    ("?" . "◻")
+       :prompt "?unread"
+       :show-target (lambda (target) "unread")
+       :action (lambda (docid msg target) (mu4e~proc-move docid nil "-S+u-N")))
+      (unmark
+       :char  " "
+       :prompt "unmark"
+       :action (mu4e-error "No action for unmarking"))
+      (action
+       :char ( "a" . "◯")
+       :prompt "action"
+       :ask-target  (lambda () (mu4e-read-option "Action: " mu4e-headers-actions))
+       :action  (lambda (docid msg actionfunc)
+                  (save-excursion
+                    (when (mu4e~headers-goto-docid docid)
+                      (mu4e-headers-action actionfunc)))))
+      (something
+       :char  ("*" . "✱")
+       :prompt "*something"
+       :action (mu4e-error "No action for deferred mark")))
+
+    "The list of all the possible marks.
+This is an alist mapping mark symbols to their properties.  The
+properties are:
+  :char (string) or (basic . fancy) The character to display in
+    the headers view. Either a single-character string, or a
+    dotted-pair cons cell where the second item will be used if
+    `mu4e-use-fancy-chars' is `t', otherwise we'll use
+    the first one. It can also be a plain string for backwards
+    compatibility since we didn't always support
+    `mu4e-use-fancy-chars' here.
+  :prompt (string) The prompt to use when asking for marks (used for
+     example when marking a whole thread)
+  :ask-target (function returning a string) Get the target.  This
+     function run once per bulk-operation, and thus is suitable
+     for user-interaction.  If nil, the target is nil.
+  :dyn-target (function from (TARGET MSG) to string).  Compute
+     the dynamic target.  This is run once per message, which is
+     passed as MSG.  The default is to just return the target.
+  :show-target (function from TARGET to string) How to display
+     the target.
+  :action (function taking (DOCID MSG TARGET)).  The action to
+     apply on the message.")
+
+  (defun yq/mu4e~proc-remove (msgid)
+    "Remove message identified by docid.
+The results are reporter through either (:update ... ) or (:error)
+sexp, which are handled my `mu4e-error-func', respectively."
+    (mu4e~proc-send-command "cmd:remove msgid:%s" msgid))
   (add-to-list 'mu4e-bookmarks
                (make-mu4e-bookmark
                 :name  "Unread filtered"
