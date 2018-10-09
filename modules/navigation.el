5672045538530f58f1c6a498069561a52e83d679
yqrashawn <namy.19@gmail.com>
2018-10-09 18:16:59 +0800

    enable repeat of s emacs mode keys

--- modules/navigation.el
+++ modules/navigation.el
@@ -275,6 +275,8 @@ _h_ ^+^ _l_ | _d_one      ^ ^  |          | _m_: matcher %-5s(ivy--matcher-desc)
 
   (global-set-key (kbd "C-x C-9 j") '+counsel-projectile-switch-buffer-next-line)
   (global-set-key (kbd "C-x C-9 k") '+counsel-projectile-switch-buffer-prev-line)
+  (global-set-key (kbd "C-M-S-s-j") '+counsel-projectile-switch-buffer-next-line)
+  (global-set-key (kbd "C-M-S-s-k") '+counsel-projectile-switch-buffer-prev-line)
   (defun yq/.emacs.d ()
     (interactive)
     (counsel-fd "" "~/.emacs.d/" nil "-t f --no-ignore-vcs -E 'straight/build/*'"))
@@ -686,13 +688,26 @@ When ARG is non-nil search in junk files."
     (interactive)
     (if (minibufferp) (ivy-done)
       (awesome-tab-forward-tab)))
+  ;; disabled, using ivy-switch-buffer
   ;; (global-set-key (kbd "C-x C-9 i") #'awesome-tab-select-beg-tab)
   ;; (global-set-key (kbd "C-x C-9 o") #'awesome-tab-select-end-tab)
-  (global-set-key (kbd "C-x C-9 h") #'awesome-tab-backward-tab)
-  (global-set-key (kbd "C-x C-9 l") '+awesome-tab-forward-tab-or-ivy-done)
   ;; (global-set-key (kbd "C-x C-9 j") '+awesome-tab-switch-group-next-line)
   ;; (global-set-key (kbd "C-x C-9 k") '+awesome-tab-switch-group-prevouse-line)
+  ;; trackpad
+  (global-set-key (kbd "s-{") #'awesome-tab-backward-tab)
+  (global-set-key (kbd "s-}") '+awesome-tab-forward-tab-or-ivy-done)
+
+  ;; terminal without repeat
+  (global-set-key (kbd "C-x C-9 h") #'awesome-tab-backward-tab)
+  (global-set-key (kbd "C-x C-9 l") '+awesome-tab-forward-tab-or-ivy-done)
   (global-set-key (kbd "C-x C-9 n") '+awesome-tab-switch-group-next-line)
   (global-set-key (kbd "C-x C-9 p") '+awesome-tab-switch-group-prevouse-line)
   (global-set-key (kbd "C-x C-9 [") #'awesome-tab-move-current-tab-to-left)
-  (global-set-key (kbd "C-x C-9 ]") #'awesome-tab-move-current-tab-to-right))
+  (global-set-key (kbd "C-x C-9 ]") #'awesome-tab-move-current-tab-to-right)
+
+  ;; gui with repeat
+  (global-set-key (kbd "C-M-S-s-h") #'awesome-tab-backward-tab)
+  (global-set-key (kbd "C-M-S-s-l") '+awesome-tab-forward-tab-or-ivy-done)
+  (global-set-key (kbd "C-M-S-s-n") '+awesome-tab-switch-group-next-line)
+  (global-set-key (kbd "C-M-S-s-p") '+awesome-tab-switch-group-prevouse-line))
+
