53b8458b2eaa34ffd3905b6f98683790cfabeed3
yqrashawn <namy.19@gmail.com>
2019-04-11 11:21:06 +0800

    disable lsp stuff for vue mode

--- modules/lang/vue.el
+++ modules/lang/vue.el
@@ -7,24 +7,24 @@
   (setq mmm-submode-decoration-level 2)
   (spacemacs/enable-flycheck 'vue-mode))
 
-(use-package lsp-mode
-  :mode ("\\.vue\\'" . lsp-mode)
-  :straight t)
+;; (use-package lsp-mode
+;;   :mode ("\\.vue\\'" . lsp-mode)
+;;   :straight t)
 
-(use-package lsp-vue
-  :straight t
-  :mode ("\\.vue\\'" . lsp-vue-mm-enable)
-  :after lsp-mode)
+;; (use-package lsp-vue
+;;   :straight t
+;;   :mode ("\\.vue\\'" . lsp-vue-mm-enable)
+;;   :after lsp-mode)
 
-(use-package company-lsp
-  :straight t
-  :after lsp-mode
-  :config
-  (spacemacs|add-company-backends
-    :backends (company-tabnine company-lsp)
-    :modes web-mode
-    :after-hook t))
+;; (use-package company-lsp
+;;   :straight t
+;;   :after lsp-mode
+;;   :config
+;;   (spacemacs|add-company-backends
+;;     :backends (company-tabnine company-lsp)
+;;     :modes web-mode
+;;     :after-hook t))
 
-(use-package lsp-ui
-  :straight t
-  :hook (lsp-mode . lsp-ui-mode))
+;; (use-package lsp-ui
+;;   :straight t
+;;   :hook (lsp-mode . lsp-ui-mode))
