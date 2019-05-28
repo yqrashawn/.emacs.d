9027d86009fd6b81510fac874cc444521597a7d6
yqrashawn <namy.19@gmail.com>
2019-05-28 14:05:34 +0800

    Fix: Org-Babel result

--- modules/better-default.el
+++ modules/better-default.el
@@ -656,6 +656,7 @@ If the universal prefix argument is used then kill the buffer too."
      ("*Messages*"                          :dedicated t   :position left   :stick t    :noselect t   :width  0.5)
      ("*Org-Babel Error Output*"            :dedicated t   :position bottom :stick nil  :noselect t   :width  0.5)
      ("*Org Babel Results*"                 :dedicated t   :position bottom :stick t    :noselect t   :width  0.5)
+     ("*Org-Babel Results*"                 :dedicated t   :position bottom :stick t    :noselect t   :width  0.5)
      ("*mu4e-update*"                       :dedicated t   :position bottom :stick nil  :noselect t)
      ("*HTTP Response*"                     :dedicated t   :position bottom :stick t    :noselect t   :height 0.4 :kill t)
      ("*compilation*"                       :dedicated t   :position bottom :stick t    :noselect t   :height 0.4 :kill t)
