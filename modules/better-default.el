503c04a57821e6f7ad120435d2b6c2536dffdb59
yqrashawn <namy.19@gmail.com>
2020-03-07 12:59:51 +0800

    Add: docker tramp

--- modules/better-default.el
+++ modules/better-default.el
@@ -315,7 +315,29 @@ file stored in the cache directory and `nil' to disable auto-saving.")
   (tramp-completion-reread-directory-timeout 900)
   :config
   (add-to-list 'tramp-connection-properties
-               (list ".*" "locale" "LC_ALL=C")))
+               (list ".*" "locale" "LC_ALL=C"))
+  ;; access file inside docker even on remote machine
+  ;; https://willschenk.com/articles/2020/tramp_tricks/
+  (push
+   (cons
+    "docker"
+    '((tramp-login-program "docker")
+      (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+      (tramp-remote-shell "/bin/sh")
+      (tramp-remote-shell-args ("-i") ("-c"))))
+   tramp-methods)
+
+  (defadvice tramp-completion-handle-file-name-all-completions
+      (around dotemacs-completion-docker activate)
+    "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+    (if (equal (ad-get-arg 1) "/docker:")
+        (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+               (dockernames (cl-remove-if-not
+                             #'(lambda (dockerline) (string-match ":$" dockerline))
+                             (split-string dockernames-raw "\n"))))
+          (setq ad-return-value dockernames))
+      ad-do-it)))
 
 (use-feature simple
   :custom
