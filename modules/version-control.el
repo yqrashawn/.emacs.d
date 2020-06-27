3ffe396ddab512a8bcbcdbda308d09b4af2b76b7
yqrashawn <namy.19@gmail.com>
2020-06-27 17:04:54 +0800

    fix: magit-cz

--- modules/version-control.el
+++ modules/version-control.el
@@ -108,8 +108,33 @@
     (when (or (s-contains? "cz-cli" s t) (s-contains? "cz-conventional-changelog" s t))
       (setq +magit-cz-found-cz-hook t))
     (when (and +magit-cz-found-cz-hook (s-contains? "move up and down to reveal" s t))
-      (setq +magit-cz-found-cz-hook nil)
-      (progn (process-send-string proc "") t)))
+      (let* ((type (alist-get
+                   (print (read-char-choice (format (propertize
+                                                     "\
+Choose commit type:
+  [c]hore
+  [f]ix
+  [C]i "
+                                                     'face
+                                                     'minibuffer-prompt))
+                                            '(?c ?f ?q)))
+                   '((?f . "fix")
+                     (?c . "chore")
+                     (?C . "ci"))))
+            (scope (read-string "Commit scope: "))
+            (breaking-change-p (y-or-n-p "Is this a breaking change?"))
+            (breaking-change-s (if breaking-change-p "y" "n"))
+            (breaking-change-body (and breaking-change-p (concat (read-string "Breaking Change Body: ") "")))
+            (breaking-change-des (and breaking-change-p (concat (read-string "Breaking Change Description: ") "")))
+            (issue-p (y-or-n-p "Is there a linking issue?"))
+            (issue-s (if issue-p "y" "n"))
+            (issue-ref (and issue-p (concat "resolve #" (read-string "Issue Ref Number: ") "")))
+            (res (concat ;; type
+                  "" scope "" "_____" "" breaking-change-s breaking-change-body breaking-change-des issue-s issue-ref)))
+        (setq +magit-cz-found-cz-hook nil)
+        (print res)
+        (process-send-string proc res))
+      t))
   (add-to-list 'magit-process-prompt-functions '+magit-handle-cz-cli))
 
 
