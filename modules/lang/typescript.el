f7f47e8fccd036678467465994e1d0932e68cdfc
yqrashawn <namy.19@gmail.com>
2020-07-02 17:57:41 +0800

    chore: typescript

--- modules/lang/typescript.el
+++ modules/lang/typescript.el
@@ -15,10 +15,19 @@ and 'typescript-formatter .")
 
 (use-package typescript-mode
   :straight t
-  :mode (("\\.ts\\'" . typescript-mode)("\\.tsx\\'" . typescript-mode))
+  :mode (("\\.ts\\'" . typescript-mode) ("\\.tsx\\'" . typescript-mode))
   :init
   (setq typescript-indent-level 2)
   (add-hook 'typescript-mode-hook 'enable-rjsx-feature-in-ts)
+  ;;; typescript-tsx-mdoe
+  ;; (progn (define-derived-mode typescript-tsx-mode web-mode "TypeScript-tsx")
+  ;;        (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-tsx-mode))
+
+  ;;        ;; (add-hook 'typescript-tsx-mode-hook #'emmet-mode)
+
+  ;;        (with-eval-after-load 'flycheck
+  ;;          (flycheck-add-mode 'typescript-tslint 'typescript-tsx-mode)
+  ;;          (flycheck-add-mode 'javascript-eslint 'typescript-tsx-mode)))
   :config
   (defun spacemacs/typescript-format ()
     "Call formatting tool specified in `typescript-fmt-tool'."
@@ -40,8 +49,25 @@ and 'typescript-formatter .")
   :defer t
   :diminish tide-mode
   :commands (typescript/jump-to-type-def)
+  :custom
+  (tide-completion-detailed t)
+  (tide-always-show-documentation t)
+  (tide-server-max-response-length 524288)
+  (tide-format-options '(:indentSize 2 :tabSize 2 :insertSpaceAfterSemicolonInForStatements t))
   :init
-  (setq tide-format-options '(:indentSize 2 :tabSize 2 :insertSpaceAfterSemicolonInForStatements t))
+  (defun +javascript-cleanup-tide-processes-h ()
+    "Clean up dangling tsserver processes if there are no more buffers with
+`tide-mode' active that belong to that server's project."
+    (when tide-mode
+      (unless (cl-loop with project-name = (tide-project-name)
+                       for buf in (delq (current-buffer) (buffer-list))
+                       if (and (buffer-local-value 'tide-mode buf)
+                               (with-current-buffer buf
+                                 (string= (tide-project-name) project-name)))
+                       return buf)
+        (kill-process (tide-current-server)))))
+
+  (add-hook 'tide-mode-hook (defl () (add-hook 'kill-buffer-hook '+javascript-cleanup-tide-processes-h nil t)))
   (add-hook 'typescript-mode-hook 'tide-hl-identifier-mode)
   (spacemacs|define-jump-handlers typescript-mode)
   ;; (spacemacs|define-jump-handlers js2-mode)
