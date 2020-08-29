d69cb507329b06b5a6460a2f4f5ef995ca8df840
yqrashawn <namy.19@gmail.com>
2020-08-29 13:18:10 +0800

    fix: disable vue-mode

--- modules/lang.el
+++ modules/lang.el
@@ -14,7 +14,7 @@
 (yq/get-modules "lang/typescript.el")
 (yq/get-modules "lang/rust.el")
 ;; (yq/get-modules "lang/c.el")
-(yq/get-modules "lang/vue.el")
+;; (yq/get-modules "lang/vue.el")
 ;; (yq/get-modules "lang/python.el")
 (yq/get-modules "lang/md.el")
 (yq/get-modules "lang/ruby.el")
