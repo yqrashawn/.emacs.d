991562838292cd13fea3b9a67d5011b8632195e2
yqrashawn <namy.19@gmail.com>
2021-03-24 11:25:26 +0800

    fix: dash-functional

--- modules/better-default.el
+++ modules/better-default.el
@@ -79,7 +79,12 @@ file stored in the cache directory and `nil' to disable auto-saving.")
 
 (use-package dash
   :straight t
+  :defer t
   :config (dash-enable-font-lock))
+
+(use-package dash-functional
+  :straight t
+  :defer t)
 
 (use-package s
   :bind
