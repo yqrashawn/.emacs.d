448d5f5d261d5dc170f9b2b9bf1e62f6ba3037c2
yqrashawn <namy.19@gmail.com>
2021-05-08 19:07:17 +0800

    chore: try marginalia

--- modules/visual.el
+++ modules/visual.el
@@ -486,3 +486,17 @@ has been changed to THEME."
 (use-package org-superstar
   :straight t
   :hook (org-mode . org-superstar-mode))
+
+(use-package marginalia
+  :straight t
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
